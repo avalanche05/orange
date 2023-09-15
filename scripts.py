d009864da43843b5f7a8a5f1ea11cec212bffaaa
Ivan Ilin <buran05@icloud.com>
2023-09-15 18:24:08 +0300

    qdw

--- scripts.py
+++ scripts.py
@@ -2,6 +2,7 @@
 from openapi_client import Configuration
 from openapi_client.api.news_controller_api import NewsControllerApi
 from openapi_client.api.stock_info_api import StockInfoApi
+from openapi_client.api.place_bid_controller_api import PlaceBidControllerApi
 from openapi_client.models import *
 from openapi_client.api_client import ApiClient
 
@@ -16,6 +17,7 @@ client = ApiClient(configuration=config,
 
 news = NewsControllerApi(api_client=client)
 stock = StockInfoApi(api_client=client)
+bid = PlaceBidControllerApi(api_client=client)
 
 r = stock.getsymbols(token)
 
@@ -28,6 +30,50 @@ def get_symbol_id_by_name(symbol_name):
             return symbol.id
 
 
+def stock_price(stock_name: str):
+    res = stock.get_sell_stock_by_symbol_id(token)
+    pass
 
 
-print(get_symbol_id_by_name('Oranges/TreeSavvy Forestry'))
+import requests
+import json
+import time
+from collections import defaultdict
+
+session = requests.session()
+session.headers.update(token="64f433d6143b064f433d6143b3")
+
+companies = defaultdict(int)
+last_news = ''
+
+stock_price("wjfekjh")
+
+
+idd = get_symbol_id_by_name("Oranges/TreeSavvy Forestry")
+r = bid.place_limit_price_buy_bid(
+    token,
+    {
+        "symbolId": int(idd),
+        "price": 160,
+        "quantity": 10
+    }
+)
+
+pass
+
+while True:
+    pass
+
+
+while True:
+    LatestNews = session.get(
+        url="https://datsorange.devteam.games/news/LatestNews"
+    )
+    data = LatestNews.json()
+    if str(data['date']) != last_news:
+        for company in data['companiesAffected']:
+            companies[company] += data['rate']
+        last_news = str(data['date'])
+    time.sleep(30)
+    max_company = min(companies.items(), key=lambda x: x[1])[0]
+    print(max_company)
